35238f3564c6f1913a81725154ef4a914697c04e
Huw Jones <huw@pexip.com>
2021-06-04 14:31:10 +0100

    Only allow google login

--- theme/pexip/login.php
+++ theme/pexip/login.php
@@ -46,142 +46,15 @@
 							</div>';
 					}
 
-					// Login page
-					if (isset($_GET['login'])) {
 					?>
-						<form action="login.php?login" method="post">
-							<div class="form-area">
-								<div class="panel-title" style="text-align:center;">
-									Login
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="username" placeholder="Username">
-									<i class="fa fa-user"></i>
-								</div>
-
-								<div class="group">
-									<input type="password" class="form-control" name="password" placeholder="Password">
-									<i class="fa fa-key"></i>
-								</div>
-
-								<div class="checkbox checkbox-primary">
-									<input id="rememberme" name="rememberme" type="checkbox" checked="">
-									<label for="rememberme">
-										<?php echo $lang['rememberme']; ?>
-									</label>
-								</div>
-
-								<input type="hidden" name="signin" value="<?php echo md5($date.$ip); ?>">
-								<button type="submit" name="submit" class="btn btn-default btn-block">Sign in</button>
-							</div>
-						</form>
 						<!-- Oauth -->
-					<?php if ($enablefb == "no") { } else { ?>
-						<a href="oauth/facebook.php?login" class="btn btn-primary btn-block">
-							<i class="fa fa-facebook"></i>Sign in with Facebook
-						</a>
-					<?php }
-						if ($enablegoog == "no") { } else { ?>
+					<?php if ($enablegoog == "no") { } else { ?>
 						<a href="oauth/google.php?login" class="btn btn-danger btn-block">
 							<i class="fa fa-google"></i>Sign in with Google
 						</a>
 					<?php } ?>
 						<!-- // -->
 
-					<?php // Registration page
-					} elseif (isset($_GET['register']))  {
-					?>
-						<form action="login.php?register" method="post">
-							<div class="form-area">
-								<div class="panel-title" style="text-align:center;">
-									Register
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="username" placeholder="Username">
-									<i class="fa fa-user"></i>
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="full" placeholder="Your Name">
-									<i class="fa fa-user-plus"></i>
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="email" placeholder="Email">
-									<i class="fa fa-envelope"></i>
-								</div>
-
-								<div class="group">
-									<input type="password" class="form-control" name="password" placeholder="Password">
-									<i class="fa fa-key"></i>
-								</div>
-
-								<input type="hidden" name="signup" value="<?php echo md5($date.$ip); ?>">
-								<button type="submit" name="submit" class="btn btn-default btn-block">Register</button>
-							</div>
-						</form>
-						<!-- Oauth -->
-					<?php if ($enablefb == "no") { } else { ?>
-						<a href="oauth/facebook.php?login" class="btn btn-primary btn-block">
-							<i class="fa fa-facebook"></i>Register with Facebook
-						</a>
-					<?php } 
-						if ($enablegoog == "no") { } else { ?>
-						<a href="oauth/google.php?login" class="btn btn-danger btn-block">
-							<i class="fa fa-google"></i>Register with Google
-						</a>
-					<?php } ?>
-						<!-- // -->
-
-					<?php // Forgot password
-					} elseif (isset($_GET['forgot']))  {
-					?>
-						<form action="login.php?forgot" method="post">
-							<div class="form-area">
-								<div class="panel-title" style="text-align:center;">
-									Forgot Password
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="email" placeholder="Enter your email address">
-									<i class="fa fa-envelope"></i>
-								</div>
-
-								<input type="hidden" name="forgot" value="<?php echo md5($date.$ip); ?>" />
-								<button type="submit" name="submit" class="btn btn-default btn-block">Submit</button>
-							</div>
-						</form>
-
-					<?php // Resend verification email
-					} elseif (isset($_GET['resend']))  {
-					?>
-						<form action="login.php?resend" method="post">
-							<div class="form-area">
-								<div class="panel-title" style="text-align:center;">
-									Resend verification email
-								</div>
-
-								<div class="group">
-									<input type="text" class="form-control" name="email" placeholder="Enter your email address">
-									<i class="fa fa-envelope"></i>
-								</div>
-
-								<input type="hidden" name="resend" value="<?php echo md5($date.$ip); ?>" />
-								<button type="submit" name="submit" class="btn btn-default btn-block">Submit</button>
-							</div>
-						</form>
-
-					<?php } else  {?>
-						<div class="panel-title" style="text-align:center;">
-							Where to?
-						</div>
-						<a href="login.php?login">Login</a><br />
-						<a href="login.php?register">Register</a> <br />
-						<a href="login.php?forgot">Forgot Password</a><br />
-						<a href="login.php?resend">Resend verification email</a><br />
-					<?php  } ?>
 					</div>
 				</div>
 			</div>
